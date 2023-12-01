b4145994367d5a81324d4d92344e2ab17de04aba
Matthew White <Matthew.White02@gmail.com>
2023-12-01 10:15:57 +0000

    Update solution to solve part 2

--- 01.py
+++ 01.py
@@ -9,15 +9,36 @@ with open("input-01.txt", "r") as file:
 # """.splitlines()][1:]
 # # Example answer  = 142
 
+# # example for part 2
+# lines = [line.strip() for line in """
+# two1nine
+# eightwothree
+# abcone2threexyz
+# xtwone3four
+# 4nineeightseven2
+# zoneight234
+# 7pqrstsixteen
+# """.splitlines()][1:]
+# # Example answer  = 281
+
 calibration_values = list()
+number_words = "zero one two three four five six seven eight nine".split()
 
 for line in lines:
     first = None
     last = None
-    for c in line:
-        if c.isdigit():
-            first = first or c
-            last = c
+    while len(line):
+        if line[0].isdigit():
+            first = first or line[0]
+            last = line[0]
+        else:
+            for word in number_words[1:]:
+                if line.startswith(word):
+                    number = number_words.index(word)
+                    first = first or str(number)
+                    last = str(number)
+                    break
+        line = line[1:]
     calibration_values.append(first + last)
 
 answer = sum(int(v) for v in calibration_values)
