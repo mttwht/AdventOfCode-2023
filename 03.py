0528cba2dcb5e5e0277a3e8f46632cd3becb4892
Matthew White <Matthew.White02@gmail.com>
2023-12-03 21:12:10 +0000

    Update solution for part 2

--- 03.py
+++ 03.py
@@ -17,24 +17,41 @@ with open("input-03.txt", "r") as file:
 # """.splitlines()][1:]
 # # Example answer  = 4361
 
+def add_num_to_gear(gears: dict[str, list[int]], gear_id: str, num: int) -> dict[str, list[int]]:
+    if gear_id not in gears:
+        gears[gear_id] = []
+    gears[gear_id].append(num)
+    return gears
+
+
+gears = {}
 
 answer = 0
 
 for i in range(len(lines)):
     line = lines[i]
     for match in re.finditer("\d+", line):
-        neighbours = ""
+        num = int(match[0])
         if i > 0:
-            neighbours += lines[i-1][max(match.start()-1, 0):min(match.end()+1, len(line)-1)]
+            for j in range(max(0, match.start() - 1), min(len(line), match.end() + 1)):
+                if lines[i-1][j] == "*":
+                    add_num_to_gear(gears, str(i-1) + "-" + str(j), int(num))
         if match.start() > 0:
-            neighbours += line[match.start()-1]
+            if lines[i][match.start() - 1] == "*":
+                add_num_to_gear(gears, str(i) + "-" + str(match.start() - 1), int(num))
         if match.end() < len(line)-1:
-            neighbours += line[match.end()]
+            if lines[i][match.end()] == "*":
+                add_num_to_gear(gears, str(i) + "-" + str(match.end()), int(num))
         if i < len(lines)-1:
-            neighbours += lines[i+1][max(match.start()-1, 0):min(match.end()+1, len(line)-1)]
-        
-        if re.search("[^0-9.]", neighbours):
-            num = int(match[0])
-            answer += num
+            for j in range(max(0, match.start() - 1), min(len(line), match.end() + 1)):
+                if lines[i+1][j] == "*":
+                    add_num_to_gear(gears, str(i+1) + "-" + str(j), int(num))
+
+for gear_id in gears:
+    gear = gears[gear_id]
+    print(gear_id, gear)
+    if len(gear) == 2:
+        print(gear[0], "*", gear[1], "=", gear[0] * gear[1])
+        answer += gear[0] * gear[1]
 
 print(answer)
