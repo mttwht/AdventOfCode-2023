451d0a664c8a8a4f45c424a8be8f54523ba9c631
Matthew White <Matthew.White02@gmail.com>
2023-12-11 20:57:37 +0100

    Update to solve part 2

--- 08.py
+++ 08.py
@@ -1,25 +1,20 @@
+import math
+
+
 with open("input-08.txt", "r") as file:
     lines = [line.strip() for line in file.readlines()]
 
 # lines = [line.strip() for line in """
-# RL
+# LR
 
-# AAA = (BBB, CCC)
-# BBB = (DDD, EEE)
-# CCC = (ZZZ, GGG)
-# DDD = (DDD, DDD)
-# EEE = (EEE, EEE)
-# GGG = (GGG, GGG)
-# ZZZ = (ZZZ, ZZZ)
-# """.splitlines()][1:]
-# # Example answer  = 2
-
-# lines = [line.strip() for line in """
-# LLR
-
-# AAA = (BBB, BBB)
-# BBB = (AAA, ZZZ)
-# ZZZ = (ZZZ, ZZZ)
+# 11A = (11B, XXX)
+# 11B = (XXX, 11Z)
+# 11Z = (11B, XXX)
+# 22A = (22B, XXX)
+# 22B = (22C, 22C)
+# 22C = (22Z, 22Z)
+# 22Z = (22B, 22B)
+# XXX = (XXX, XXX)
 # """.splitlines()][1:]
 # # Example answer  = 6
 
@@ -37,18 +32,29 @@ def parse_lines(lines):
     nodes = parse_nodes(lines[2:])
     return instructions, nodes
 
-START, END = "AAA", "ZZZ"
 
-current_node = START
 
 step_count = 0
 instructions, nodes = parse_lines(lines)
-while current_node != END:
+current_nodes = [n for n in nodes if n.endswith("A")]
+end_steps = [[] for n in current_nodes]
+stop = False
+
+while len([n for n in current_nodes if n.endswith("Z")]) < len(current_nodes):
     instruction = instructions[step_count % len(instructions)]
     if instruction == 'L':
-        current_node = nodes[current_node][0]
+        current_nodes = [nodes[n][0] for n in current_nodes]
     elif instruction == 'R':
-        current_node = nodes[current_node][1]
+        current_nodes = [nodes[n][1] for n in current_nodes]
     step_count += 1
 
-print(step_count)
+    for i, n in enumerate(current_nodes):
+        if n.endswith('Z'):
+            end_steps[i].append(step_count)
+            if min([len(s) for s in end_steps]) >= 1:
+                stop = True
+                break
+    if stop:
+        break
+
+print(math.lcm(*[es[0] for es in end_steps]))
